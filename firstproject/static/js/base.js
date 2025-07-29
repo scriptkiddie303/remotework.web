c62ee92d997021ed56763fb6005c8ed05e4d9816
scriptkiddie303 <zaryabawan34325@gmail.com>
2025-07-29 22:03:50 +0500

    Delete firstproject directory

--- firstproject/static/js/base.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.querySelector('#dark-light button');
-const body = document.body;
-
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('light-theme');
-});
-
--- firstproject/static/js/index.js
+++
@@ -1,8 +0,0 @@
-function toggleMode() {
-    document.body.classList.toggle('dark-mode');
-}
-
-function toggleMenu() {
-    const nav = document.getElementById('nav-links');
-    nav.classList.toggle('show');
-}
